2493406d818174bca6f7efb65ec87835df695063
Maksim <maksim_belotelov@mail.ru>
2022-12-06 20:03:58 +0300

    add Ex27

---
+++ Ex27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает на вход число и выдает сумму цифр в числе
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {num} равна {SumOfDig(num)}");
+
+int SumOfDig(int num)
+{
+    int sum=0;
+    for (int i=num; i!=0; i/=10)
+        sum+=i%10;
+    return sum;
+}
