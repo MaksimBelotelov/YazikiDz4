e09b34fdb8d946fc566e74b39fc977076b57802b
Maksim <maksim_belotelov@mail.ru>
2022-12-06 19:51:44 +0300

    add Ex25

---
+++ Ex25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+
+Console.Clear();
+
+Console.Write("Введите число (А): ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите натуральную степень, в которую нужно возвести число (В): ");
+int b=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{a} ^ {b} = {Expon(a,b)}");
+
+int Expon(int a, int b)
+{
+    int result=a;
+    
+    for(int i=1; i<b; i++)
+        result*=a;
+    return result;
+}
